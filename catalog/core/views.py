a525fd9f5ba1a0db9012a3460cda506d97ff85c0
Denis Voskvitsov <denis.voskvitsov@gmail.com>
2011-11-21 21:20:10 +0400

    Shop page.

--- catalog/core/views.py
+++ catalog/core/views.py
@@ -2,7 +2,7 @@
 
 from django.views.generic.simple import direct_to_template
 from django.http import HttpResponse, Http404
-from django.shortcuts import redirect
+from django.shortcuts import redirect, get_object_or_404
 from django.core.urlresolvers import reverse
 from django.conf import settings
 
@@ -32,11 +32,23 @@ def home(request):
 
 
 def index(request):
-    return HttpResponse("Ok")
+    return redirect(reverse("home"))
 
 
 def shop_view(request, shop_id):
-    return HttpResponse("Ok")
+    shop = get_object_or_404(Shop, pk=shop_id)
+    goods = GoodInfo.objects.filter(shop = shop)
+
+    # get all producers in the shop
+    goods_ids = list(goods.values_list("good__id", flat=True))
+    producers = Good.objects.filter(id__in = goods_ids).values_list("producer__name", flat=True).distinct()
+
+    extra = {
+        "shop": shop,
+        "producers": producers,
+        "goods": goods
+        }
+    return direct_to_template(request, "shop.html", extra)
 
 
 def good_in_shop(request, good_id, shop_id):
